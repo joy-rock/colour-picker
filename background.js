fd642908db9aa406c7f84606acc30a391c78b11a
joy-rock <91815607+joy-rock@users.noreply.github.com>
2023-10-25 23:52:18 +0530

    Update background.js
    The chrome.contextMenus.create function is used to create a context menu item with the title 'Pick a Color' that is available in all contexts.

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
+// Create a context menu item titled 'Pick a Color' that is available in all contexts.
 chrome.contextMenus.create({
     title: 'Pick a Color',
     contexts: ['all'],
     onclick: function () {
+        // Query for the active tab in the current window
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            // Send a message to the content script of the active tab to show the popup
             chrome.tabs.sendMessage(tabs[0].id, { action: 'show_popup' });
         });
     }
